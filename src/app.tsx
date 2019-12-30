7093a83cb8f77660f14b9aa533499df8d3d558b5
Kazuya Tsubone <kazuya.tsubone@nulab-inc.com>
2019-12-30 21:50:42 +0900

    modify .gitignore

--- src/app.tsx
+++ src/app.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Hello from './Hello'
 
-class App extends React.Component {
-  render() {
-    return (
-      <>
-        <h1>Hello <Hello name='React' /> + <Hello name='TypeScript' />!</h1>
-      </>
-    )
-  }
-}
+const App: React.FC<React.Props<{}>> = () => (
+  <>
+    <svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512">
+      <circle cx="150" cy="200" r="100" fill="#06f"></circle>
+    </svg>
+    <h1>Hello <Hello name='React' /> + <Hello name='TypeScript' />!</h1>
+  </>
+);
 
 ReactDOM.render(<App/>, document.querySelector('#app'))
